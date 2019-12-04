d6914ec54f23e7bc971c02b6c40a5ca7173f589b
Serabass <serabass565@gmail.com>
2019-12-04 20:00:29 +0600

    upd

--- src/util.ts
+++ src/util.ts
@@ -1,23 +1,43 @@
 import * as path from "path";
 import { Processor } from "./processor";
 
+/**
+ * Выводим JSON в консоль
+ * @param obj Значение
+ * @param indent Отступ
+ */
 export function json(obj: any, indent = 2) {
   // console.log(JSON.stringify(obj, null, indent));
 }
 
+/**
+ * Формируем буфер из набора Hex-значений
+ * @param data Строка с Hex-значениями
+ */
 export function buf(data: string | Buffer) {
   if (typeof data === "string") {
     data = Buffer.from(data.replace(/[\s|]+/g, ""), "hex");
   }
 
-  // fs.readFileSync('main.scm').slice(0x9AE4, 0x9AE4 + 8);
   return data;
 }
 
+/**
+ * Формируем путь к файлу
+ *
+ * @param specFileName Имя файла
+ */
 export function pathFix(specFileName: string) {
   return path.join(".", "examples", specFileName + ".go");
 }
 
+/**
+ * Грузим парсер
+ *
+ * @param scriptPath Путь к файлу
+ * @param buffer Входящий буфер
+ * @param src Исходник PEG
+ */
 export function load(
   scriptPath: string,
   buffer: string | Buffer,
