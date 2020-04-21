370d201fb3932deda171f8ce166ad4f80f234a93
Serabass <serabass565@gmail.com>
2020-04-21 11:22:16 +0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,3 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.[t]s?(x)", "**/?(*.)+(spec|test).[t]s?(x)"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "node", "go", "pegjs"]
 };
-
-///
